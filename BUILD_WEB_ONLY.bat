ba809e4a097fe1f966c948f72bc455b017c6feed
PiggeGnidde <89888134+PiggeGnidde@users.noreply.github.com>
2026-08-16 18:16:02 +0200

    Route web-only build through Geometry V1a enhancer

--- BUILD_WEB_ONLY.bat
+++ BUILD_WEB_ONLY.bat
@@ -1,6 +1,6 @@
 @echo off
 chcp 65001 >nul
 cd /d "%~dp0"
-py -3 src\07_build_web.py
-if errorlevel 1 python src\07_build_web.py
+py -3 src\build_web_only.py
+if errorlevel 1 python src\build_web_only.py
 pause
